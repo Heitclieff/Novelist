cf6681987ad31f5f609975be8615b5b673f19426
Heitclieff <nagisa.1881@gmail.com>
2023-07-16 01:51:40 +0700

    Updated LeaderBoard page UI and Change Elements.

--- app/pages/[stack]/leaderboard/Leaderboard.tsx
+++ app/pages/[stack]/leaderboard/Leaderboard.tsx
@@ -2,142 +2,134 @@ import React,{useContext, useRef , useEffect} from 'react'
 import { View } from 'react-native'
 import { Box,Text,VStack } from 'native-base'
 import { ThemeContext } from '../../../../systems/Theme/ThemeProvider'
-import {Platform } from 'react-native'
-import { ScrollView } from 'react-native'
-import { ImageHeaderScrollView, TriggeringView } from 'react-native-image-header-scroll-view'
-import { Image } from 'expo-image'
+import {Platform , StyleSheet } from 'react-native'
 import Leadheader from '../../../components/[stack]/leaderboard/leadheader/Leadheader'
 import LeadContainer from '../../../components/[stack]/leaderboard/LeadContainer'
 import { useDispatch , useSelector } from 'react-redux'
+
 import { ThunkDispatch } from 'redux-thunk'
 import { AnyAction } from 'redux'
 import { RootState } from '../../../../systems/redux/reducer'
 import { getuserData } from '../../../../systems/redux/action'
-import ParallaxScrollView from 'react-native-parallax-scroll-view';
-import { useHeaderHeight } from '@react-navigation/elements'
-
+import { ImageBackground } from 'react-native'
+import Animated from 'react-native-reanimated'
+import { BlurView } from 'expo-blur'
 
 interface pageProps {}
 
 const Leaderboard: React.FC <pageProps> = () => {
     const theme:any = useContext(ThemeContext);
-    const MIN_HEIGHT = Platform.OS == 'ios' ? 90 : 55;
-    const MAX_HEIGHT  = 400;
-    const STICKY_HEADER_HEIGHT = 90;
+   
     const dispatch =  useDispatch<ThunkDispatch<RootState, unknown, AnyAction>>();
     const userdata = useSelector((state:any) => state.userData)
     const isReduxLoaded = useSelector((state:RootState) =>state.isuserLoaded )
 
-    const headerHeight = useHeaderHeight();
+    const MAX_HEIGHT  = 410;
+    const HEADER_HEIGHT_NARROWED = 90;
+    const HEADER_HEIGHT_EXPANDED = 300; 
+   
+    const scrollY = useRef(new Animated.Value(0)).current;
+    const AnimatedBackground = Animated.createAnimatedComponent(ImageBackground)
+    const AnimatedBlurView = Animated.createAnimatedComponent(BlurView);
+
     useEffect(() => {
         dispatch(getuserData());
     } , [dispatch , isReduxLoaded])
 
   return (
-    <ParallaxScrollView 
-    parallaxHeaderHeight={MAX_HEIGHT}
-    stickyHeaderHeight={headerHeight}
-    backgroundColor = {'transparent'}
-    fadeOutForeground={true} 
-    fadeOutStickyHeader = {false}
-    fadeOutHeader  ={false}
-    backgroundScrollSpeed={2}
-    backgroundSpeed = {10}
-    showsVerticalScrollIndicator =  {false}
-    renderBackground = {() => (
-        <Box w = '100%' h = '100%' bg = 'black'>
-            <Image
-            id='background-images'
-            style={{ width: '100%', height: '100%', opacity: 0.6, resizeMode: 'cover' }}
-            contentFit='cover'
-            source={userdata[0].image}
-            alt="images"
-            />
-        </Box>
-        
-    )}
+    <Box flex = {1} >
+        <Box w = '100%' h = {MAX_HEIGHT}  position={'absolute'} justifyContent={'center'}>
+         <Box w = '100%' h = '100%'  position = 'absolute' top = {0}>
+            <Animated.View style = {[{width : '100%' , height : '100%'}, { transform: [
+                  {
+                    translateY: scrollY.interpolate({
+                      inputRange: [0, 300],
+                      outputRange: [0, -100],
+                      extrapolate: 'clamp',
+                    }),
+                  },
+                ], }]}>
+                <AnimatedBackground
+                    id='background-images'
+                    source={{uri :userdata[0].image}}
+                    alt="images"
+                    style={{ 
+                        width: '100%', 
+                        height: '100%', 
+                        opacity: 1,
+                        position: 'relative',
 
-    
-    renderStickyHeader = {() => (
-        <Box w=  '100%' h ='100%' bg = 'black' overflow={'hidden'} position = 'relative'> 
-        <Box w = '100%' h= '265' position = 'absolute' top = {0}>
-            <Image
-                id='background-images'
-                style={{ width: '100%', height: '100%', opacity: 0.6, transform: [{ translateY: 0 }] }}
-                contentFit='cover'
-                source={userdata[0].image}
-                alt="images"
-            />     
-        </Box>
-            
-    </Box>
-    
-    )}
-
-
-    renderHeader = {() => (
-        <Box w=  '100%' h ='100%' bg = 'black' overflow={'hidden'} position = 'relative'> 
-        <Box w = '100%' h= '265' position = 'absolute' top = {0}>
-            <Image
-                id='background-images'
-                style={{ width: '100%', height: '100%', opacity: 0.6, transform: [{ translateY: 0 }] }}
-                contentFit='cover'
-                source={userdata[0].image}
-                alt="images"
-            />     
-        </Box>
-            
-    </Box>
-    
-    )}
-
-    renderForeground={() => (
-        <Box w='100%' h='100%'
-        bg={{
-            linearGradient: {
-                colors: ['transparent', 'transparent', theme.Bg.base],
-                start: [1, 0, 0],
-                end: [0, 0, 0],
-            },
-        }}>
-            <Leadheader data = {userdata[0]}/>
+                        transform: [{
+                            scale : scrollY.interpolate({
+                                inputRange : [-500 ,0],
+                                outputRange : [5,1],
+                                extrapolateLeft : 'extend',
+                                extrapolateRight : 'clamp',
+                            })
+                        }]
+                        }
+                    }
+                    > 
+                    <Box w ='100%' h= '100%' bg = 'black' opacity={0.6} position={'absolute'} zIndex={5}></Box>
+                    <Box w = '100%' h = '100%' position={'absolute'} zIndex={10} bottom = {1}  bg={{
+                        linearGradient: {
+                            colors: ['transparent', 'transparent', theme.Bg.base],
+                            start: [1, 0, 0],
+                            end: [0, 0, 0],
+                        },
+                    }}>
+                    </Box>
+                    <AnimatedBlurView
+                    tint="dark"
+                    intensity={96}
+                    style = {{
+                        ...StyleSheet.absoluteFillObject,
+                        zIndex : 2,
+                        opacity : scrollY.interpolate({
+                            inputRange : [-50 , 0 , 50 ,100],
+                            outputRange : [1,0,0,1]
+                        })
+                    }}
+                    />
+                </AnimatedBackground>   
+            </Animated.View>
          </Box>
-       
-    )}>
-    <Box w = '100%' h = '100%' >
-        <VStack bg = {theme.Bg.base} pt = {6} pb = {5} alignItems={'center'} space = {3}>
-                { [0,0,0,0,0,0,0,0,0,0,0,0].map((item ,index:number) =>
-                    <LeadContainer index = {index + 4} key = {index} />
-                )
-                }
-        </VStack>
+        <Leadheader data = {userdata[0]} />
+        
     </Box>
-    </ParallaxScrollView>
-   
-        /* <ImageHeaderScrollView
-            maxHeight={MAX_HEIGHT}
-            minHeight={MIN_HEIGHT}
-            scrollViewBackgroundColor= {theme.Bg.tabbar}
-            showsVerticalScrollIndicator = {false}
-            renderHeader={() => {
-                return(
-                    <Leadheader data = {userdata[0]} />   
-                )
+    <Box flex=  {1}>
+        <Animated.ScrollView
+        showsVerticalScrollIndicator = {false}
+        onScroll={Animated.event([
+            {
+                nativeEvent : {
+                    contentOffset : {
+                        y: scrollY
+                    }
+                }
             }
-            }>
-            <TriggeringView>
-                <VStack mt = {6} alignItems={'center'} space = {3}>
-                    { [0,0,0,0,0,0,0].map((item ,index:number) =>
-                        <LeadContainer index = {index + 4} />
-                    )
+        ],
+        {
+            useNativeDriver : true
+        })}
+        scrollEventThrottle={16}
+        style ={{
+            zIndex : 3 ,
+            marginTop : HEADER_HEIGHT_NARROWED ,
+            paddingTop : HEADER_HEIGHT_EXPANDED,
+        }}
+        >
+                <VStack  bg = {theme.Bg.base} borderTopLeftRadius={'lg'} borderTopRightRadius={'lg'} pt = {6} pb = {HEADER_HEIGHT_EXPANDED} alignItems={'center'} space = {3}>
+                        { [0,0,0,0,0,0,0,0,0].map((item ,index:number) =>
+                        <LeadContainer index = {index + 4} key = {index} />
+                        )
                     }
                 </VStack>
-            </TriggeringView>
+        </Animated.ScrollView>
+    </Box>
     
-        
-        </ImageHeaderScrollView> */
-
-    
+    </Box>
+  
   )
 }
 
