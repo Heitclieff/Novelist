e0d6d3ad9da8274ef9bdbf2df76c0af8a212fc87
Kittituch.pulp <nagisa.1881@gmail.com>
2023-08-26 00:27:12 +0700

    Updated TeamMember Container Add Pressable to container.

--- app/components/creater/[container]/TeamMember.tsx
+++ app/components/creater/[container]/TeamMember.tsx
@@ -1,5 +1,5 @@
 import React,{useContext} from 'react'
-import { Box , HStack, VStack , Text } from 'native-base';
+import { Box , HStack, VStack , Text, Pressable } from 'native-base';
 import { ThemeContext } from '../../../../systems/Theme/ThemeProvider';
 import { Image } from 'expo-image';
 
@@ -10,25 +10,36 @@ interface containerProps {
 const TeamMember : React.FC <containerProps> = ({data , isleader = false}) => {
      const theme:any = useContext(ThemeContext)
   return (
-    <HStack m = {1} p = {3} space = {3} bg = {theme.Bg.container} rounded={'full'} alignItems={'center'} position={'relative'}>
-          <Box w = {10} h = {10} rounded={'full'} bg = 'gray.200' overflow={'hidden'}>
-               <Image
-               id = 'profile-image'
-               style={{width : '100%' ,height : '100%'}}
-               source={data.image}
-               />
-          </Box>
-          <VStack>
-               <Text color = {theme.Text.base} fontWeight={'semibold'}>{data.username}</Text>
-               <Text color = {theme.Text.base} fontSize={'xs'}>{data.email}</Text>
-          </VStack>
-          {isleader &&
-           <Box position={'absolute'} right={6}>
-               <Text color={theme.Text.description} fontSize={'xs'} >you</Text>
-          </Box>
-          }
+     <Pressable>
+     {({
+         isHovered,
+         isFocused,
+         isPressed
+     }) => {
+          return(
+          <HStack m = {1} p = {3} space = {3} bg = {isPressed ? theme.Bg.action : isHovered ? theme.Bg.action  : theme.Bg.container} rounded={'full'} alignItems={'center'} position={'relative'}>
+               <Box w = {10} h = {10} rounded={'full'} bg = 'gray.200' overflow={'hidden'}>
+                    <Image
+                    id = 'profile-image'
+                    style={{width : '100%' ,height : '100%'}}
+                    source={data.image}
+                    />
+               </Box>
+               <VStack>
+                    <Text color = {theme.Text.base} fontWeight={'semibold'}>{data.username}</Text>
+                    <Text color = {theme.Text.base} fontSize={'xs'}>{data.email}</Text>
+               </VStack>
+               {isleader &&
+               <Box position={'absolute'} right={6}>
+                    <Text color={theme.Text.description} fontSize={'xs'} >you</Text>
+               </Box>
+               }
          
-    </HStack>
+          </HStack>  
+          )
+     }}
+    
+    </Pressable>
   )
 }
 
